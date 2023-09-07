46d7236c1dcb15b0b8f9a68b0617e4081000b7c0
Nate Orange <cc.lemonhead@gmail.com>
2023-09-06 20:49:15 -0400

    added imagemachine2

--- app/utils/CommonObjects/imagemachine.mjs
+++ app/utils/CommonObjects/imagemachine.mjs
@@ -75,11 +75,12 @@ async function getImageUrlsForDestinatioinCB(destinationText = "Visit Africa"){
 export async function checkPixaBay(searchString,numImages = 1){
     console.log(`Getting ${numImages} images for ${searchString}`)
     let result = []
-    const url = `https://google.com/`
+    const url = `https://google.com/` 
+    console.log(url);
     // const url = `https://pixabay.com/images/search/${searchString}/`
-    const data = await fetch(url);
-    const resultData = await data.text();
-    console.log(resultData);
+    // const data = await fetch(url);
+    // const resultData = await data.text();
+    // console.log(resultData);
 //     const $ = load(resultData);
 //     const images = $("img");
 //     console.log(images.length);
