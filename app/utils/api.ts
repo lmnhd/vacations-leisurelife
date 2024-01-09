a86b6f841e86697f4158e3c61311e23982080b0b
Nate Orange <cc.lemonhead@gmail.com>
2024-01-09 18:44:57 -0500

    changed cbPicks function to cache:no-store2

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -41,7 +41,7 @@ async function checkForStoredAIResponse(
   });
   return storedResponse;
 }
-async function deleteStoredData(tableValueName:string){
+async function deleteStoredData(tableValueName: string) {
   const storedResponse = prismadb.aIAssist.deleteMany({
     where: {
       functionId: tableValueName,
@@ -63,30 +63,32 @@ export async function aiAssistBackOff(
   data: string,
   componentId: string,
   functionId: string,
-  deleteData:string = ''
-){
+  deleteData: string = ""
+) {
   try {
-    const response:string = await backOff(() => aiAssist(instructions,data,componentId,functionId,deleteData)) 
+    const response: string = await backOff(() =>
+      aiAssist(instructions, data, componentId, functionId, deleteData)
+    );
     return response;
   } catch (error) {
     console.error(error);
     //return error;
   }
 }
- async function aiAssist(
+async function aiAssist(
   instructions: string,
   data: string,
   componentId: string,
   functionId: string,
-  deleteData:string = ''
+  deleteData: string = ""
 ) {
-  if(deleteData == ''){}else{
-    console.log("deleting stored data...",deleteData);
+  if (deleteData == "") {
+  } else {
+    console.log("deleting stored data...", deleteData);
     await deleteStoredData(deleteData);
     //return 'deleted';
   }
-  
-  
+
   const message = `${instructions} : """${data}"""`;
   console.log(message);
   //return
@@ -98,32 +100,34 @@ export async function aiAssistBackOff(
   );
   if (storedResponse.length > 0) {
     console.log("stored response found");
+    console.log(storedResponse[0].response)
     return storedResponse[0].response;
+  }else{
+    console.log("no stored response found");
+    return "no response found";
   }
-  if(process.env.NODE_ENV == 'development'){
+  if (process.env.NODE_ENV == "development") {
     console.log("no stored response found, querying openai...");
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-  const userMessage: any = {
-    role: "user",
-    content: message,
-  };
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [userMessage],
-    max_tokens: 1000,
-  });
-  const res = response.choices[0].message.content || "error";
-  console.log(res);
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    const userMessage: any = {
+      role: "user",
+      content: message,
+    };
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [userMessage],
+      max_tokens: 1000,
+    });
+    const res = response.choices[0].message.content || "error";
+    console.log(res);
 
-  if (res !== "error") {
-    console.log("storing response...");
-    storeAIResponse(data, res, componentId, functionId);
-  } return res;
-
+    if (res !== "error") {
+      console.log("storing response...");
+      storeAIResponse(data, res, componentId, functionId);
+    }
+    return res;
   }
-  
-return "no response found"
-  //console.log(response.choices[0].message.content)
- 
-}
 
+  return "no response found";
+  //console.log(response.choices[0].message.content)
+}
